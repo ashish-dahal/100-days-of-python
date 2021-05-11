7a284cbac5be6b2970c9c51466fe68c4232d6ada
Ashish Dahal <ashishdahal1997@gmail.com>
2021-05-11 10:41:01 +0545

    Day 2 of 100 days of python

---
+++ day-2.py
@@ -0,0 +1,10 @@
+#Program to calculate how many days, weeks and months is left in your life
+
+age = input("What is your current age?")
+
+age = 90 - age
+days = age * 365
+months = age * 12
+weeks = age * 52
+
+print(f"You have {days} days, {weeks} weeks and {months} months left")
