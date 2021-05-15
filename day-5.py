3de408a95018e5a0a30cfced08d72ed3efa2ffaa
Ashish Dahal <ashishdahal1997@gmail.com>
2021-05-15 11:37:27 +0545

    Add fizz buzz program - Day 5

---
+++ day-5.py
@@ -0,0 +1,12 @@
+# Fizz Buzz game
+output = ""
+for item in range(1, 101):
+    output = item
+    if item % 3 == 0:
+        output = "Fizz"
+    if item % 5 == 0:
+        try:
+            output += " Buzz"
+        except:
+            output = "Buzz"
+    print(output)
