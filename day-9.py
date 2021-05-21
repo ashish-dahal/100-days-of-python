5edf5ade11cc25990a6d3714cefc1aef4d516743
Ashish Dahal <ashishdahal1997@gmail.com>
2021-05-21 12:03:16 +0545

    Refactor the program to calculate highest auction bidder

--- day-9.py
+++ day-9.py
@@ -8,17 +8,9 @@ print("Welcome to the secret auction program.")
 while have_more_bidders == "yes":
     name = input("What is your name? : ")
     bid = int(input("What is your bid? : "))
-
     bids.update({name: bid})
-
     have_more_bidders = input("Are there any other bidders? Type 'yes' or 'no': ")
 
-max_bid = 0
-max_index = 0
-
-for bidder in bids:
-    if bids[bidder] > max_bid:
-        max_bid = bids[bidder]
-        max_bidder = bidder
-
-print(f"The winner is {max_bidder} with a bid of {max_bid}.")
+print(
+    f"The winner is {max(bids, key=bids.get)} with a bid of {max(list(bids.values()))}."
+)
